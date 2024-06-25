7f6d79705b3b60f93c165ff9718419b750aade09
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2024-06-25 17:28:31 -0600

    Arrays con For anidados

--- Hola Mundo/src/Arrays.java
+++ Hola Mundo/src/Arrays.java
@@ -1,11 +1,61 @@
-import com.sun.org.apache.xpath.internal.operations.String;
-
 public class Arrays {
     public static void main(String[] args) {
-        String [] androidVersions = new String[17];
+        String[] androidVersions = new String[17];
         String days[] = new String[7];
         String[][] cities = new String[4][2];
         int [][][] numbers = new int [2][2][2];
         int [][][][] numbers4 = new int [2][2][2][2];
+
+        androidVersions[0] = "Apple pie";
+        androidVersions[1] = "Banana Bread";
+        androidVersions[2] = "Cupcake";
+        androidVersions[3] = "Donut";
+
+        for (int i = 0; i < androidVersions.length ; i++) {
+            System.out.println(androidVersions[i]);
+        }
+
+        //FOREACH
+        for(String andoirdVersion : androidVersions){
+            System.out.println(andoirdVersion);
+        }
+
+        /*System.out.println(androidVersions[0]);
+        System.out.println(androidVersions[1]);
+        System.out.println(androidVersions[2]);
+        System.out.println(androidVersions[3]);*/
+
+        System.out.println();
+        System.out.println();
+
+        cities[0][0] = "Colombia";
+        cities[0][1] = "Medellin";
+        cities[1][0] = "Colombia";
+        cities[1][1] = "Bogotá";
+        cities[2][0] = "México";
+        cities[2][1] = "Guadalajara";
+        cities[3][0] = "Mexico";
+        cities[3][1] = "CDMX";
+
+        for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+
+        for (String[]pair : cities){
+            for (String name:pair){
+                System.out.println(name);
+            }
+        }
+
+        /*System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);*/
     }
 }
