bd43d1bab49366a2c5260a871d92e2663b1d5cd1
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2024-06-25 15:48:39 -0600

    Javadoc
    Se implementó la manera de crear los javadocs

--- Hola Mundo/src/Funciones.java
+++ Hola Mundo/src/Funciones.java
@@ -16,6 +16,7 @@ public class Funciones {
 
         System.out.println("Pesos Mexicanos a Dolares: " + convertToDollar(1000,"MXN"));
         System.out.println("Pesos Colmbianos a Dolares: " + convertToDollar(1000,"COP"));
+        
     }
 
     public static double circuleArea(double radio){
@@ -27,6 +28,14 @@ public class Funciones {
     public static double sphereVolume(double radio){
         return (4/3) * Math.PI * Math.pow(radio,3);
     }
+
+    /**
+     * Descripcion: Función que especificando su moneda convierte una cantidad de dinero a Dolares
+     *
+     * @param quantity Cantidad de Dinero
+     * @param currency Tipo de moneda: solo acepta MXN o COP
+     * @return quantity Devuelve la cantidad actualizada en Dolares
+     * */
     public static double convertToDollar(double quantity, String currency){
         //MXN COP
         switch (currency){
