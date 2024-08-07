6f878a698a87eb75bed4d0f91ae973f524e897c3
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2024-08-07 15:17:20 -0600

    Final (Constantes en JAVA)

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -2,16 +2,6 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Pedro Hernández");
-        myDoctor.name = "Felipe Cazin";
-        myDoctor.showName();
-        myDoctor.showId();
-
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctorAnn.showId();
-
         showMenu();
-
     }
 }
