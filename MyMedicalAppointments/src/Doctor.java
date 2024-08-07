de84eb0f85b334a8454e70d863ba8e6f271c1637
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2024-08-07 12:44:26 -0600

    Generando elementos estaticos

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,12 +1,24 @@
-import java.sql.SQLOutput;
-
 public class Doctor {
-    int id;
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
+
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Dr asingado es "+name);
+    }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,17 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Pedro Hernández");
         myDoctor.name = "Felipe Cazin";
         myDoctor.showName();
+        myDoctor.showId();
+
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+
+        showMenu();
+
     }
 }
