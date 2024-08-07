a0690bcd9dfd5817f6bc200eddc597e06f77650c
PedroHernandez654 <hernandezdzulpedro2@gmail.com>
2024-08-07 17:17:33 -0600

    Variable Vs Objeto
    Nota: Si se realizar un "=" de un objeto a otro, realmente lo que se está igualando es la "ruta" del heap donde están guardados los elementos del objeto, por lo tanto si algún cambio ocurre en cualquier objeto se replica en el otro

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -7,12 +7,33 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
+        int i = 0;
+        int b = 2;
+        b = i;
+        String name = "Ann";
+
         Patient patient = new Patient("Alejandra","alejandra@gmail.com");
-        patient.setWeight(54.6);
+        Patient patient2 = new Patient("Anahi","anahi@gmail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient.setName("Manuela");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        /*patient.setWeight(54.6);
         System.out.println(patient.getWeight());
 
         patient.setPhoneNumber("12345678");
-        System.out.println(patient.getPhoneNumber());
+        System.out.println(patient.getPhoneNumber());*/
 
         //patient.weight = 60.5; //Kg
         //patient.height = 1.65; // Mts
